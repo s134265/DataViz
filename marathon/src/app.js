f55a9fbf9b5a5426344c6b062f898c808c0fe691
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2018-02-24 22:38:34 +0100

    added transitions, show/hide functionality to marathon plot

--- marathon/src/app.js
+++ marathon/src/app.js
@@ -2,13 +2,24 @@
 var w = 750;
 var h = 570;
 var padding = 40;
+var margin = { x: 80, y: 50 };
+
+var transitionOrder = {
+  first: 0,
+  second: 1500,
+  third: 3000,
+  fourth: 4500,
+};
 
 // Define SVG
 var svg = d3.select('#marathon-viz')
   .append('svg')
   .attr('id', 'marathon-svg')
-  .attr('width', w)
-  .attr('height', h);
+  .attr('width', w + margin.x)
+  .attr('height', h + margin.y)
+  // .attr('height', h)
+  .append('g')
+  .attr('transform', `translate(${margin.x / 2}, 0)`);
 
 // Read data
 var parseYear = d3.timeParse('%Y');
@@ -29,7 +40,7 @@ var formatFinishingTime = function (time) {
   time -= m;
   var s = Math.floor(time * 60);
 
-  return `${h}h${m}m${s}s`;
+  return `${h}h ${m}m ${s}s`;
 };
 
 var rowConverter = function (r) {
@@ -75,11 +86,11 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     .attr('transform', `translate(0, ${h - padding})`)
     .call(xAxis);
 
-  // svg.append('text')
-  //   .attr('class', 'axis-label')
-  //   .attr('x', w / 2)
-  //   .attr('y', h - 10)
-  //   .text('Year');
+  svg.append('text')
+    .attr('class', 'axis-label')
+    .attr('x', w / 2)
+    .attr('y', h + 5)
+    .text('Year');
 
   var yAxis = d3.axisLeft()
     .scale(yScale);
@@ -89,12 +100,13 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     .attr('transform', `translate(${padding}, 0)`)
     .call(yAxis);
 
-  // svg.append('text')
-  //   .attr('class', 'axis-label')
-  //   .attr('transform', 'rotate(-90)')
-  //   .attr('y', 20)
-  //   .attr('x', -h / 2)
-  //   .text('Time in minutes');
+  svg.append('text')
+    .attr('class', 'axis-label')
+    .attr('transform', 'rotate(-90)')
+    // swap directions because element was rotated
+    .attr('y', -10)
+    .attr('x', -h / 2)
+    .text('Time in minutes');
 
   var maleGroup = svg.append('g')
     .attr('id', 'male-runners');
@@ -106,16 +118,36 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     .x(d => xScale(d.year))
     .y(d => yScale(d.time));
 
-  // Draw paths to connect the datapoints. Create these before datapoints
-  // to draw datapoints on top.
   var malePath = maleGroup.append('path')
     .attr('class', 'male')
     .datum(maleRunners)
     .attr('d', line);
+
   var femalePath = femaleGroup.append('path')
     .attr('class', 'female')
     .datum(femaleRunners)
     .attr('d', line);
+
+  var showPath = function (path) {
+    var totalLength = path.node().getTotalLength();
+
+    path.attr('stroke-dasharray', totalLength + ' ' + totalLength)
+      .attr('stroke-dashoffset', totalLength)
+      .transition()
+      .duration(1500)
+      .delay(transitionOrder.second)
+      .attr('stroke-dashoffset', 0);
+  };
+
+  var hidePath = function (path) {
+    var totalLength = path.node().getTotalLength();
+
+    path.attr('stroke-dashoffset', 0)
+      .transition()
+      .duration(1500)
+      .delay(transitionOrder.second)
+      .attr('stroke-dashoffset', -totalLength)
+  }
 
   // Plot point properties
   var maleProps = {
@@ -124,7 +156,6 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
       h: 5,
     },
   };
-
   var femaleProps = {
     dim: {
       r: 2.5,
@@ -139,6 +170,8 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     var yPosition = boundingRect.top;
 
     var tooltip = d3.select('#tooltip');
+
+    // apply offset to center tooltip horizontically on plotpoint
     var ttXOffset = -parseInt(tooltip.style('width')) / 2;
 
     // Style tooltip
@@ -172,6 +205,7 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     .enter()
     .append('rect')
     .attr('class', 'male plot-point')
+    .attr('opacity', 0)
     // x and y positions are adjusted so that rectangle is centered on data value
     .attr('x', (d) => {
       return xScale(d.year) - .5 * maleProps.dim.w;
@@ -189,15 +223,31 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
     .enter()
     .append('circle')
     .attr('class', 'female plot-point')
+    .attr('opacity', 0)
     .attr('cx', d => xScale(d.year))
     .attr('cy', d => yScale(d.time))
     .attr('r', femaleProps.dim.r)
     .on('mouseover', mouseOverHandler)
     .on('mouseout', mouseOutHandler);
 
-  // Add regression lines
+  var showPoints = function (points) {
+    points.transition()
+      .delay((d, i) => 10 * i)
+      .ease(d3.easeCircleOut)
+      .attr('opacity', 1);
+  };
 
-  // Initialize regressor functions using data from Python
+  var hidePoints = function (points) {
+    points.transition()
+      .duration(transitionOrder.third)
+      .delay((d, i) => 10 * i)
+      .ease(d3.easeCircleIn)
+      .attr('opacity', 0);
+  };
+
+  // REGRESSION //
+
+  // Initialize linear functions using data from Python and currying in JS
   var regression = function regressionClosure(coeff, intersect) {
     return function performRegression(x) {
       return coeff * x.getFullYear() + intersect;
@@ -217,49 +267,144 @@ d3.csv('./data/olympic_merged.csv', rowConverter, (dataset) => {
   var oldestFemale = d3.min(f, d => d.year);
   var newestFemale = d3.max(f, d => d.year);
 
+  // Define start and end points for regression lines 
+  // for both groups of plot points
+  var regressionPoints = {
+    male: {
+      start: { x: xScale(oldestMale), y: yScale(regressMale(oldestMale)) },
+      end: { x: xScale(newestMale), y: yScale(regressMale(newestMale)) },
+    },
+    female: {
+      start: { x: xScale(oldestFemale), y: yScale(regressFemale(oldestFemale)) },
+      end: { x: xScale(newestFemale), y: yScale(regressFemale(newestFemale)) },
+    },
+  };
+
+  // Initialize lines. These aren't shown until showRegressionLine is called
   var maleRegressionLine = maleGroup.append('line')
     .attr('id', 'male-regression-line')
-    .attr('class', 'male regression')
-    .attr('x1', xScale(oldestMale))
-    .attr('x2', xScale(newestMale))
-    .attr('y1', yScale(regressMale(oldestMale)))
-    .attr('y2', yScale(regressMale(newestMale)));
+    .attr('class', 'male regression');
 
   var femaleRegressionLine = femaleGroup.append('line')
     .attr('id', 'female-regression-line')
-    .attr('class', 'female regression')
-    .attr('x1', xScale(oldestFemale))
-    .attr('x2', xScale(newestFemale))
-    .attr('y1', yScale(regressFemale(oldestFemale)))
-    .attr('y2', yScale(regressFemale(newestFemale)));
+    .attr('class', 'female regression');
+
+  var showRegressionLine = function (rl, startPoint, endPoint) {
+    rl.attr('x1', startPoint.x)
+      .attr('y1', startPoint.y)
+      .attr('x2', startPoint.x)
+      .attr('y2', startPoint.y)
+      .transition()
+      .duration(1500)
+      .delay(transitionOrder.third)
+      .attr('x2', endPoint.x)
+      .attr('y2', endPoint.y);
+  };
+
+  var hideRegressionLine = function (rl, startPoint, endPoint) {
+    rl.transition()
+      .duration(1500)
+      // .delay(transitionOrder.third)
+      .attr('x1', endPoint.x)
+      .attr('y1', endPoint.y)
+      .attr('x2', endPoint.x)
+      .attr('y2', endPoint.y);
+  };
+
+  // Labeling
+  var maleGroupLabel = maleGroup.append('text')
+    .text('Men')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 11)
+    .attr('x', 200)
+    .attr('y', h - 120)
+    .attr('opacity', 0);
+
+  var femaleGroupLabel = femaleGroup.append('text')
+    .text('Women')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 11)
+    .attr('x', 500)
+    .attr('y', h - 400)
+    .attr('opacity', 0);
+
+  var showGroupLabel = function (gl) {
+    gl.transition()
+      .duration(1000)
+      .attr('opacity', 1);
+  };
+
+  var hideGroupLabel = function (gl) {
+    gl.transition()
+      .duration(1000)
+      .delay(transitionOrder.fourth)
+      .attr('opacity', 0);
+  }
 
   var maleHidden = false;
   var femaleHidden = false;
 
+  var showMaleGroup = function () {
+    showPoints(malePoints);
+    showRegressionLine(maleRegressionLine, regressionPoints.male.start, regressionPoints.male.end);
+    showPath(malePath);
+    showGroupLabel(maleGroupLabel);
+  };
+
+  var hideMaleGroup = function () {
+    hidePoints(malePoints);
+    hideRegressionLine(maleRegressionLine, regressionPoints.male.start, regressionPoints.male.end);
+    hidePath(malePath);
+    hideGroupLabel(maleGroupLabel);
+  };
+
+  var showFemaleGroup = function () {
+    showPoints(femalePoints);
+    showRegressionLine(femaleRegressionLine, regressionPoints.female.start, regressionPoints.female.end);
+    showPath(femalePath);
+    showGroupLabel(femaleGroupLabel);
+  };
+
+  var hideFemaleGroup = function () {
+    hidePoints(femalePoints);
+    hideRegressionLine(femaleRegressionLine, regressionPoints.female.start, regressionPoints.female.end);
+    hidePath(femalePath);
+    hideGroupLabel(femaleGroupLabel);
+  };
+
+  // Position legend table
+  d3.select('body').select('#legend')
+    .style('right', 0)
+    .style('top', parseInt((h + margin.y) / 2) + 'px')
+    .classed('hidden', false);
+
   d3.select('body').select('#legend-male')
-    .on('click', () => {
+    .on('click', (d, i, nodes) => {
       maleHidden = !maleHidden;
-      maleGroup.classed('hidden', maleHidden);
+      // Show grey font if runners are hidden, black if not
+      d3.select(nodes[i]).style('color', maleHidden ? 'gray' : 'black')
+
+      if (maleHidden) {
+        hideMaleGroup();
+      } else {
+        showMaleGroup();
+      }
     });
 
   d3.select('body').select('#legend-female')
-    .on('click', () => {
+    .on('click', (d, i, nodes) => {
       femaleHidden = !femaleHidden;
-      femaleGroup.classed('hidden', femaleHidden);
+      // Show grey font if runners are hidden, black if not
+      d3.select(nodes[i]).style('color', femaleHidden ? 'gray' : 'black');
+
+      if (femaleHidden) {
+        hideFemaleGroup();
+      } else {
+        showFemaleGroup();
+      }
     });
 
-  // Labeling
-  maleGroup.append('text')
-    .text('Men')
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', 13)
-    .attr('x', 200)
-    .attr('y', h - 120);
+  showMaleGroup();
+  showFemaleGroup();
 
-  femaleGroup.append('text')
-    .text('Women')
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', 13)
-    .attr('x', 500)
-    .attr('y', h - 400);
 });
